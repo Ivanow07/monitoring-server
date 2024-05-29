74e7a2089bee1f3e744b0f9fd264b351e10cf1d5
Ivanow07_ <138402069+Ivanow07@users.noreply.github.com>
2024-05-29 23:08:26 +0300

    Update ram_usage.sh

--- ram_usage.sh
+++ ram_usage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-ram_used=$(free -m | awk '/Mem:/ {print $3}')
-ram_available=$(free -m | awk '/Mem:/ {print $7}')
-ram_total=$(free -m | awk '/Mem:/ {print $2}')
+# Get RAM usage details
+ram_total=$(free -m | awk '/^Mem:/{print $2}')
+ram_used=$(free -m | awk '/^Mem:/{print $3}')
+ram_available=$(free -m | awk '/^Mem:/{print $7}')
 
-curl -X POST -H "Content-Type: application/json" -d "{\"used\":\"$ram_used\", \"available\":\"$ram_available\", \"total\":\"$ram_total\"}" http://130.61.241.28:8000/update/ram
+# Output results
+echo "$ram_used,$ram_available,$ram_total"
