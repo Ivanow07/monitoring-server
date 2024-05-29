644344c819d0075c60c3c8be0416060007b34fc4
Ivanow07_ <138402069+Ivanow07@users.noreply.github.com>
2024-05-29 22:27:59 +0300

    Update network_usage.sh

--- network_usage.sh
+++ network_usage.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-network_in=$(cat /proc/net/dev | grep eth0 | awk '{print $2}')
-network_out=$(cat /proc/net/dev | grep eth0 | awk '{print $10}')
+# Network interface (you might need to change eth0 to your actual network interface)
+interface=eth0
 
-echo "$network_in,$network_out"
+network_in=$(cat /sys/class/net/$interface/statistics/rx_bytes)
+network_out=$(cat /sys/class/net/$interface/statistics/tx_bytes)
+
+curl -X POST -H "Content-Type: application/json" -d "{\"in\":\"$network_in\", \"out\":\"$network_out\"}" http://130.61.241.28:8000/update/network
