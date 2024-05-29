0e0f8adf3c82a26cfe6bc459bb2489f3a11e84d3
Ivanow07_ <138402069+Ivanow07@users.noreply.github.com>
2024-05-29 23:10:13 +0300

    Update disk_usage.sh

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-disk_used=$(df -h / | awk 'NR==2 {print $3}')
-disk_available=$(df -h / | awk 'NR==2 {print $4}')
-disk_total=$(df -h / | awk 'NR==2 {print $2}')
+# Get Disk usage details
+disk_total=$(df -h / | grep / | awk '{print $2}')
+disk_used=$(df -h / | grep / | awk '{print $3}')
+disk_available=$(df -h / | grep / | awk '{print $4}')
 
-curl -X POST -H "Content-Type: application/json" -d "{\"used\":\"$disk_used\", \"available\":\"$disk_available\", \"total\":\"$disk_total\"}" http://130.61.241.28:8000/update/disk
+# Output results
+echo "$disk_used,$disk_available,$disk_total"
