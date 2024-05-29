cdd6159df3bbcc30f94941e0dda1e66569f691b1
Ivanow07_ <138402069+Ivanow07@users.noreply.github.com>
2024-05-29 22:26:14 +0300

    Update cpu_usage.sh

--- cpu_usage.sh
+++ cpu_usage.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cpu_model=$(cat /proc/cpuinfo | grep 'model name' | uniq | awk -F ':' '{print $2}' | xargs)
-cpu_cores=$(nproc)
+cpu_model=$(lscpu | grep 'Model name' | awk -F: '{print $2}' | xargs)
+cpu_cores=$(lscpu | grep '^CPU(s):' | awk -F: '{print $2}' | xargs)
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
 
-echo "$cpu_model"
-echo "$cpu_cores"
-echo "$cpu_usage"
+curl -X POST -H "Content-Type: application/json" -d "{\"model\":\"$cpu_model\", \"cores\":\"$cpu_cores\", \"usage\":\"$cpu_usage\"}" http://130.61.241.28:8000/update/cpu
